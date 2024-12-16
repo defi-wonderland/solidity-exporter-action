52b257b69b6b0c577fa73198e9bf6289551bde50
teddy <teddy@defi.sucks>
2024-12-16 14:20:57 -0300

    test: extra testcases from code review

--- test/unit/parseGitmodulesDependencies.spec.ts
+++ test/unit/parseGitmodulesDependencies.spec.ts
@@ -9,12 +9,14 @@ describe('parseGitmodulesDependencies', () => {
   after(() => {
     mock.restore();
   });
+
   describe('with no gitmodules file', function () {
     it('should return an empty dependency list', function () {
       const parsedDependencies = parseGitmodulesDependencies();
       expect(parsedDependencies).to.deep.eq({});
     });
   });
+
   describe('with an empty file', function () {
     before(() => {
       mock({
@@ -26,6 +28,7 @@ describe('parseGitmodulesDependencies', () => {
       expect(parsedDependencies).to.deep.eq({});
     });
   });
+
   describe('with a file with several dependencies', function () {
     before(() => {
       mock({
@@ -40,6 +43,57 @@ describe('parseGitmodulesDependencies', () => {
       });
     });
     it('should be able to parse them', function () {
+      const parsedDependencies = parseGitmodulesDependencies();
+      expect(parsedDependencies['forge-std']).to.eq('https://github.com/foundry-rs/forge-std');
+      expect(parsedDependencies['openzeppelin-contracts']).to.eq(
+        'https://github.com/OpenZeppelin/openzeppelin-contracts',
+      );
+      expect(Object.keys(parsedDependencies)).to.have.length(2);
+    });
+  });
+
+  describe('with a file with extra (valid) keys besides path and url', function () {
+    before(() => {
+      mock({
+        '.gitmodules': `
+      [submodule "lib/forge-std"]
+        path = lib/forge-std
+        branch = .
+        url = https://github.com/foundry-rs/forge-std
+      [submodule "lib/openzeppelin-contracts"]
+        ignore = all
+        path = lib/openzeppelin-contracts
+        url = https://github.com/OpenZeppelin/openzeppelin-contracts
+      `,
+      });
+    });
+    it('should be able to parse them', function () {
+      const parsedDependencies = parseGitmodulesDependencies();
+      expect(parsedDependencies['forge-std']).to.eq('https://github.com/foundry-rs/forge-std');
+      expect(parsedDependencies['openzeppelin-contracts']).to.eq(
+        'https://github.com/OpenZeppelin/openzeppelin-contracts',
+      );
+      expect(Object.keys(parsedDependencies)).to.have.length(2);
+    });
+  });
+
+  describe('with a file where subsection keys are specified in multiple subsection definitions', function () {
+    before(() => {
+      // this is valid syntax according to git, but our regex 'parser' can't handle it
+      mock({
+        '.gitmodules': `
+      [submodule "lib/openzeppelin-contracts"]
+        url = https://github.com/OpenZeppelin/openzeppelin-contracts
+      [submodule "lib/forge-std"]
+        path = lib/forge-std
+      [submodule "lib/openzeppelin-contracts"]
+        path = lib/openzeppelin-contracts
+      [submodule "lib/forge-std"]
+        url = https://github.com/foundry-rs/forge-std
+      `,
+      });
+    });
+    it.skip('should be able to parse them', function () {
       const parsedDependencies = parseGitmodulesDependencies();
       expect(parsedDependencies['forge-std']).to.eq('https://github.com/foundry-rs/forge-std');
       expect(parsedDependencies['openzeppelin-contracts']).to.eq(
